55ac06ae9b1574011ef49b6526fe760f72cd5403
Houadef Salih <salih.houadef@gmail.com>
2023-08-18 16:06:47 +0100

    make copy and paste global

--- nvim/lua/ezio/remap.lua
+++ nvim/lua/ezio/remap.lua
@@ -4,4 +4,4 @@ vim.keymap.set("n","<leader>f",vim.cmd.Ex)
 vim.keymap.set("n","<leader>p",'"+gp')
 vim.keymap.set("n","<leader>b",':Prettier<CR>')
 vim.api.nvim_set_keymap('n', '<space>o', ':!xdg-open % &<CR>', { noremap = true })
-
+vim.api.nvim_command('set clipboard+=unnamedplus')
